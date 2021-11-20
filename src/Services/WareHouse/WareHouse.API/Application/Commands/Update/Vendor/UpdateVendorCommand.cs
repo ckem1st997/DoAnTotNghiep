b4317e466a62e838e35c65294d394b6167673460
ckem1st997 <1721050681@student.humg.edu.vn>
2021-11-20 12:15:07 +0700

    update

---
+++ src/Services/WareHouse/WareHouse.API/Application/Commands/Update/Vendor/UpdateVendorCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+using MediatR;
+using WareHouse.API.Application.Commands.Models.Vendor;
+using WareHouse.API.Application.Commands.Models.WareHouse;
+
+namespace WareHouse.API.Application.Commands.Update.Vendor
+{
+    public class UpdateVendorCommand: IRequest<bool>
+    {
+        [DataMember]
+        public VendorCommands VendorCommands { get; set; }
+    }
+}
