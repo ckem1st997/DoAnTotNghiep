491cb4814b7fd325a170ca12ac6235888962f822
ckem1st997 <hopxc1997@gmail.com>
2022-04-10 13:06:51 +0700

    update

--- src/Services/WareHouse/WareHouse.API/Controllers/BaseController/BaseControllerWareHouse.cs
+++ src/Services/WareHouse/WareHouse.API/Controllers/BaseController/BaseControllerWareHouse.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WareHouse.API.Controllers.BaseController
 {
-  //  [Authorize(Roles = "User,Admin,Manager")]
+    [Authorize(Roles = "User,Admin,Manager")]
     [Route("api/v{v:apiVersion}/[controller]")]
     [ApiController]
     public class BaseControllerWareHouse : ControllerBase
