7a87126b8b6d0da5b75e7a989d15998299ae32c3
NGUYEN KHA HOP <1721050681@student.humg.edu.vn>
2022-11-07 14:49:31 +0700

    update

--- src/Share.BaseCore/Authozire/ConfigureServices/ServiceAuthorize.cs
+++ src/Share.BaseCore/Authozire/ConfigureServices/ServiceAuthorize.cs
@@ -1,20 +1,98 @@
-﻿using MediatR;
+﻿using AutoMapper.Configuration;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace Share.BaseCore.Authozire.ConfigureServices
 {
     public static class ServiceAuthorize
     {
-        public static void AddBehavior(this IServiceCollection services)
+        public static void InitAppSettings(this IServiceCollection services,IConfiguration configuration)
         {
-            //   services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            CommonHelper.BaseConfig = configuration.GetSection("JWT").Get<BaseSsoConfig>();
+        }
+        public static void AddApiAuthentication(this IServiceCollection services)
+        {
+            services.AddApiVersioning(x =>
+            {
+                x.DefaultApiVersion = new ApiVersion(1, 0);
+                x.AssumeDefaultVersionWhenUnspecified = true;
+                x.ReportApiVersions = true;
+            });
+            //
+            services.AddHttpContextAccessor();
             services.AddScoped<IGetClaims, GetClaims>();
             services.AddScoped<IAuthozireExtensionForMaster, AuthozireExtensionForMaster>();
+            var ssoConfig = CommonHelper.BaseConfig;
+
+
+            // Adding Authentication  
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            // Adding Jwt Bearer  
+            .AddJwtBearer(options =>
+            {
+                options.Authority = ssoConfig.Authority;
+                options.RequireHttpsMetadata = ssoConfig.RequireHttpsMetadata;
+
+                options.Audience = ssoConfig.ClientId;
+
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                   // ValidateIssuer = false,
+
+                    ValidateAudience = false,
+
+                    //ValidAudience = AuthozireStringHelper.JWT.ValidAudience,
+                    //ValidIssuer = AuthozireStringHelper.JWT.ValidAudience,
+                    //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(AuthozireStringHelper.JWT.Secret))
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        // If the request is for our hub...
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) &&
+                            (path.StartsWithSegments("/signalr")))
+                        {
+                            // Read the token out of the query string
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        }
+        public static void AddApiCors(this IServiceCollection services)
+        {
+            services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+            }));
         }
     }
 }
