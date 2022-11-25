9fa903d34511dbd4a1a104c03567fc54054b0dba
ckem1st997 <hopxc1997@gmail.com>
2022-11-25 21:09:16 +0700

    update

--- src/ShareImplemention/UserInfomationService.cs
+++ src/ShareImplemention/UserInfomationService.cs
@@ -45,6 +45,24 @@ namespace ShareImplemention
                     return true;
             }
 
+            // viết thêm phần check key role
+            // cache list role
+            // mỗi lần vô đây thì bước đầu là lấy list role từ cache, nếu không lấy list từ grpc hoặc check bằng grpc
+            // xem active có true không, nếu true thì tiếp tục check, nếu false thì tức là key này không hoạt động
+            // action dùng key này sẽ không cần xác thực mà cho phép truy cập luôn, hàm này trả về true luôn
+            // nếu là true tức là cần xác thực, check xem request có xác thực chưa, nếu chưa thì return 401
+
+            var listRoleCache = new List<string>();
+            // listRoleCache là null thì get by grpc
+            var getRole = listRoleCache.FirstOrDefault(x => x.Equals(authRole));
+            // không có trong list
+            if (getRole is null)
+                return false;
+            // key không hoạt động và không có ai xác thực
+            if (getRole.Equals("active false") && idUser.Trim().Equals("IsAuthenticatedFalse"))
+                return true;
+
+
             SaveChange res = await _client.CheckAuthozireByUserIdAndRoleKeyAsync(new CheckAuthozireByUserIdAndRoleKeyModel()
             {
                 RoleKey = authRole,
