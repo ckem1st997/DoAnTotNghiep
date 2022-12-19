83abb72454568387ef00fa7cf968ac59034bcc98
NGUYEN KHA HOP <1721050681@student.humg.edu.vn>
2022-12-19 15:27:01 +0700

    update

--- src/Services/WareHouse/WareHouse.API/Controllers/BaseController/BasePublicController.cs
+++ src/Services/WareHouse/WareHouse.API/Controllers/BaseController/BasePublicController.cs
@@ -8,5 +8,68 @@ namespace WareHouse.API.Controllers.BaseController
     [ApiController]
     public class BasePublicControllerWareHouse : ControllerBase
     {
+        //or stogare => create PARTITION
+
+        // crate PARTITION
+
+
+        //        USE[WarehouseManagement]
+        //GO
+        //BEGIN TRANSACTION
+        //CREATE PARTITION FUNCTION[VoucherDateInward] (datetime2(0)) AS RANGE LEFT FOR VALUES()
+
+
+        //CREATE PARTITION SCHEME[VoucherDateInwardS] AS PARTITION[VoucherDateInward] TO([PRIMARY])
+
+
+        //ALTER TABLE[dbo].[InwardDetail]
+        //        DROP CONSTRAINT[FK_InwardDetails_PK_Inward]
+
+
+
+
+
+
+        //ALTER TABLE[dbo].[Inward] DROP CONSTRAINT[PK__Inward__3214EC07BF1204AC] WITH(ONLINE = OFF)
+
+
+        //SET ANSI_PADDING ON
+
+        //ALTER TABLE[dbo].[Inward] ADD PRIMARY KEY NONCLUSTERED
+        //(
+        //    [Id] ASC
+        //)WITH(PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON[PRIMARY]
+
+
+        //CREATE CLUSTERED INDEX[ClusteredIndex_on_VoucherDateInwardS_638070540877190714] ON[dbo].[Inward]
+        //        (
+
+        //    [VoucherDate]
+        //) WITH(SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON[VoucherDateInwardS] ([VoucherDate])
+
+
+        //DROP INDEX[ClusteredIndex_on_VoucherDateInwardS_638070540877190714] ON[dbo].[Inward]
+
+
+
+
+        //        ALTER TABLE[dbo].[InwardDetail] WITH CHECK ADD CONSTRAINT[FK_InwardDetails_PK_Inward] FOREIGN KEY([InwardId])
+        //REFERENCES[dbo].[Inward]
+        //        ([Id])
+        //ON DELETE CASCADE
+        //ALTER TABLE[dbo].[InwardDetail]
+        //        CHECK CONSTRAINT[FK_InwardDetails_PK_Inward]
+
+
+
+
+
+
+
+
+        //     COMMIT TRANSACTION
+        //
+
+
     }
 }
