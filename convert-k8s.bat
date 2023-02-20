f53220329dcc9dea699e0ad5738b1743c6b48909
ckem1st997 <hopxc1997@gmail.com>
2023-02-20 20:18:35 +0700

    d

--- convert-k8s.bat
+++ convert-k8s.bat
@@ -1,3 +1,5 @@
-curl -L https://github.com/kubernetes/kompose/releases/download/v1.26.0/kompose-windows-amd64.exe -o kompose.exe
-mkdir exportk8s
-kompose -v convert -f docker-compose.yml -o exportk8s
+@REM curl -L https://github.com/kubernetes/kompose/releases/download/v1.26.0/kompose-windows-amd64.exe -o kompose.exe
+@REM mkdir exportk8s
+@REM kompose -v convert -f docker-compose.yml -o exportk8s
+
+kompose -v convert -f docker-compose.yml -o exportk8s.yaml
