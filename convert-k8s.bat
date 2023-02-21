3fb3bfa200e9d1c2ff46fe59834f7d524b565872
ckem1st997 <hopxc1997@gmail.com>
2023-02-21 19:32:45 +0700

    update

--- convert-k8s.bat
+++ convert-k8s.bat
@@ -10,3 +10,5 @@ kompose -v convert -f docker-compose.yml -o k8s.yaml
 @REM kubectl get events
 @REM kubectl get pods
 @REM kubectl describe pod  master-api-7b69f6dfd7-t294j
+@REM kubectl get svc,po,deploy
+@REM helm uninstall master-api
