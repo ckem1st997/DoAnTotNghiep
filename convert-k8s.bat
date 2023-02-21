84139464e6c52f69e1abf2a8bc86ab275bd514eb
ckem1st997 <hopxc1997@gmail.com>
2023-02-21 09:54:34 +0700

    update

--- convert-k8s.bat
+++ convert-k8s.bat
@@ -2,4 +2,7 @@
 @REM mkdir exportk8s
 @REM kompose -v convert -f docker-compose.yml -o exportk8s
 
-kompose -v convert -f docker-compose.yml -o exportk8s.yaml
+kompose -v convert -f docker-compose.yml -o k8s.yaml
+
+@REM đường dẫn tới file đường dẫn tới chart chưa file
+@REM helm install -f k8s-helm/master-api/values.yaml master-api k8s-helm/master-api
