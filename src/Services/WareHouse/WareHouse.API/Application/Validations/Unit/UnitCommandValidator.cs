3a5821a43ae69f477ff7002235037f7fec14faea
NGUYEN KHA HOP <1721050681@student.humg.edu.vn>
2023-07-05 17:28:47 +0700

    update Validator FluentValidation equal dbcontext

--- src/Services/WareHouse/WareHouse.API/Application/Validations/Unit/UnitCommandValidator.cs
+++ src/Services/WareHouse/WareHouse.API/Application/Validations/Unit/UnitCommandValidator.cs
@@ -1,7 +1,10 @@
 using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using WareHouse.API.Application.Commands.Models;
 
@@ -14,5 +17,32 @@ namespace WareHouse.API.Application.Validations
             RuleFor(order => order.UnitName).NotEmpty().WithMessage(ValidatorString.GetMessageNotNull("Tên sản phẩm"));
             RuleFor(order => order.UnitName).NotNull().WithMessage(ValidatorString.GetMessageNotNull("Tên sản phẩm"));
         }
+
+        private readonly IMediator _dbContext;
+
+        public UnitCommandValidator(IMediator dbContext)
+        {
+            _dbContext = dbContext;
+
+            RuleFor(order => order.UnitName)
+                .NotEmpty().WithMessage(ValidatorString.GetMessageNotNull("Tên sản phẩm"))
+                .NotNull().WithMessage(ValidatorString.GetMessageNotNull("Tên sản phẩm"));
+
+            RuleFor(order => order.Id)
+                .MustAsync(UnitIdExists).WithMessage("Id đầu vào không tồn tại trong cơ sở dữ liệu.");
+        }
+
+        private async Task<bool> UnitIdExists(string Id, CancellationToken cancellationToken)
+        {
+            var existingUnit = await _dbContext.Send(new UnitCommands()
+            {
+                Id = Id
+            });
+
+            return existingUnit != null;
+        }
+
+
+
     }
 }
